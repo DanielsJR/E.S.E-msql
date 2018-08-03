01697b3cf95592ec8c7db08bdd1d6fc55b0119f0
DanielsJR <d.rubio@outlook.com>
2018-08-03 13:21:15 -0400

    MYSQL XD

--- src/main/java/nx/ESE/EseMysqlApplication.java
+++ src/main/java/nx/ESE/EseMysqlApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EseMsqlApplication {
+public class EseMysqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EseMsqlApplication.class, args);
+		SpringApplication.run(EseMysqlApplication.class, args);
 	}
 }
